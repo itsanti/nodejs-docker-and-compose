e5c8b88339ecb49b1817ea22b0b44dc68f9e9d86
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2025-11-17 17:38:53 +0300

    Fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
     origin: [
       'http://localhost:8081',
-      'https://kupi-frontend.nomorepartiessbs.ru/',
+      'https://kupi-frontend.nomorepartiessbs.ru',
     ],
     credentials: true,
   });
